c64f4a67b58f966ce29af1a27111653da3746078
shaileshgaddam22@gmail.com <shaileshgaddam22@gmail.com>
2023-07-07 19:51:21 +0530

    Setup and requirements Completed

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setup.tools import setup, find_packages
+from typing import List
+
+HYPEN_DOT_E = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    with open("requirements.txt") as f:
+        '''
+        This function will return a list of requirements
+        '''
+        requirements = []
+        with open(file_path) as file_obj:
+            requirements = file_obj.readlines()
+            [req.replace("\n", " ")for req in requirements.txt]
+
+
+            if HYPEN_DOT_E in requirements:
+                requirements.remove(HYPEN_DOT_E)
+
+        return requirements
+
+setup(
+    name = "Practice Ml Project",
+    version = "0.0.1",
+    author = "Shailesh Gaddam"
+    author_email = "shaileshgaddam22@gmail.com"
+    find_packages = find_packages(),
+    install_requires = get_requirements("requirements.txt")
+)

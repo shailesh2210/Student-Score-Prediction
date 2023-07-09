eb22ca59583df8458ed87709bd042533c5d73f75
shaileshgaddam22@gmail.com <shaileshgaddam22@gmail.com>
2023-07-09 16:48:41 +0530

    Notebook

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setup.tools import setup, find_packages
+from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_DOT_E = "-e ."
+HYPEN_E_DOT = "-e ."
 
 def get_requirements(file_path:str)->List[str]:
     '''
@@ -10,20 +10,20 @@ def get_requirements(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        [req.replace("\n", " ")for req in requirements.txt]
+        requirements = [req.replace("\n", "") for req in requirements]
 
-
-        if HYPEN_DOT_E in requirements:
-                requirements.remove(HYPEN_DOT_E)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
 setup(
-    name = "Practice Ml Project",
-    version = "0.0.1",
-    author = "Shailesh Gaddam"
-    author_email = "shaileshgaddam22@gmail.com"
-    find_packages = find_packages(),
-    install_requires = get_requirements("requirements.txt")
+name = "Practice Ml Project",
+version = "0.0.1",
+author = "Shailesh Gaddam",
+author_email = "shaileshgaddam22@gmail.com",
+packages = find_packages(),
+install_requires = get_requirements("requirements.txt")
+
 )
 

4c0272eaa4b48547d860aaf74456c4c263b7e248
shaileshgaddam22@gmail.com <shaileshgaddam22@gmail.com>
2023-07-14 17:40:11 +0530

    in process

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,10 +18,11 @@ from catboost import CatBoostRegressor
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object , evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    model_trainer_file_path = os.path.join("artifacts","")
+    model_trainer_file_path = os.path.join("artifacts","model.pkl")
 
 class ModelTrainer():
     def __init__(self):
@@ -31,11 +32,30 @@ class ModelTrainer():
         try:
             logging.info("train test split")
 
-            models = (
-                ("Linear Regression", LinearRegression(),
-                 "Random Forest", RandomForestRegressor(),
-                 )
+            x_train , y_train , x_test , y_test = (
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1]
             )
+
+            models = {
+                "Linear Regression", LinearRegression(),
+                "Random Forest", RandomForestRegressor(),
+                "DecisionTree Regressor", DecisionTreeRegressor(),
+                "CatBoost Regressor", CatBoostRegressor(),
+                "XgBoost Regressor", XgBoostRegressor(),
+                "GradientBoosting Regressor", GradientBoostingRegressor(),
+                "Ada Boost Regressor", AdaBoostRegressor(),
+                "Extra Trees Regressor", ExtraTreesRegressor()
+                
+            }
+
+            model_report:dict = evaluate_models(x_train=x_train , y_train=y_train ,
+                                    x_test = x_test ,y_test=y_test , models=models )
+            
+
+
         except Exception as e:
             raise CustomException(e,sys)
         
